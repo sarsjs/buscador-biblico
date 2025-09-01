e7b6cefddfa1c9bcbcedfc9b934eefbaf6ce5e63
sarsjs <114009921+sarsjs@users.noreply.github.com>
2025-08-31 19:25:20 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Este es un service worker básico.
+// Por ahora, solo es necesario para que la PWA sea instalable
+// y para que Bubblewrap funcione.
+self.addEventListener('fetch', event => {
+  // No hace nada por ahora, pero su existencia es clave.
+});
+
